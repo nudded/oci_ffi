672216759d8d8fea79081adc713c587361ae2588
Toon Willems <willemstoon@gmail.com>
2017-01-14 14:53:11 +0000

    create enum file, add OCIEnvNlsCreate

---
+++ src/enums.rs
@@ -0,0 +1,32 @@
+#![allow(non_camel_case_types)]
+
+// Standard way for defining structs that have no fields in the external C library
+pub enum OCIEnv {}
+
+// all possible error codes as defined in the documentation
+pub enum OCIError {
+    OCI_SUCCESS = 0,
+    OCI_SUCCESS_WITH_INFO = 1,
+    OCI_NO_DATA = 100,
+    OCI_ERROR = -1,
+    OCI_INVALID_HANDLE = -2,
+    OCI_NEED_DATA = 99,
+    OCI_STILL_EXECUTING = -3123,
+    OCI_CONTINUE = -24200,
+    OCI_ROWCBK_DONE = -24201,
+}
+
+// all possible modes as defined
+pub enum OCIMode {
+    OCI_DEFAULT = 0,
+    OCI_THREADED = 1,
+    OCI_OBJECT = 2,
+    OCI_EVENTS = 4,
+    OCI_NO_UCB = 40,
+    OCI_ENV_NO_MUTEX = 80,
+    OCI_NEW_LENGTH_SEMANTICS = 20000,
+    OCI_SUPPRESS_NLS_VALIDATION = 100000,
+    OCI_NCHAR_LITERAL_REPLACE_ON = 400000,
+    OCI_NCHAR_LITERAL_REPLACE_OFF = 800000,
+    OCI_ENABLE_NLS_VALIDATION = 1000000,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,46 @@
 extern crate libc;
-use libc::{c_int, c_ulong, c_void, size_t};
+
+use libc::{c_int, c_ulong, c_ushort, c_void, size_t};
 
 use std::ptr;
+
+mod enums;
+use enums::*;
 
 // Add standard oracle types for more compliant external FFI description
 #[allow(non_camel_case_types)]
 type ub4 = c_ulong;
 #[allow(non_camel_case_types)]
+type ub2 = c_ushort;
+#[allow(non_camel_case_types)]
 type sword = c_int;
 
-// Standard way for defining structs that have no fields in the external C library
-pub enum OCIEnv {}
-
-// all possible error codes as defined in the documentation
-pub enum OCIError {
-    OCI_SUCCESS = 0,
-    OCI_SUCCESS_WITH_INFO = 1,
-    OCI_NO_DATA = 100,
-    OCI_ERROR = -1,
-    OCI_INVALID_HANDLE = -2,
-    OCI_NEED_DATA = 99,
-    OCI_STILL_EXECUTING = -3123,
-    OCI_CONTINUE = -24200,
-    OCI_ROWCBK_DONE = -24201,
-}
 
 #[link(name = "clntsh")]
 extern "system" {
+    /* create the OCIEnv handle! */
     fn OCIEnvCreate(envp: *mut *mut OCIEnv,
                     mode: ub4,
                     ctxp: *const c_void,
-                    malocfp: *const extern "system" fn(ctxp: *mut c_void, size: size_t) -> *mut c_void,
-                    ralocfp: *const extern "system" fn(ctxp: *mut c_void, memptr: *mut c_void, newsize: size_t) -> *mut c_void,
-                    mfreep: *const extern "system" fn(ctxp: *mut c_void, memptr: *mut c_void),
+                    malocfp: *const extern "system" fn(ctxp: *mut c_void,
+                                                       size: size_t) -> *mut c_void,
+                    ralocfp: *const extern "system" fn(ctxp: *mut c_void,
+                                                       memptr: *mut c_void,
+                                                       newsize: size_t) -> *mut c_void,
+                    mfreep: *const extern "system" fn(ctxp: *mut c_void,
+                                                      memptr: *mut c_void),
                     xtramemsz: size_t,
                     usermempp: *mut *mut c_void) -> sword;
 }
 
-pub fn oci_env_create() -> *mut OCIEnv {
+pub fn oci_env_create(mode: OCIMode) -> *mut OCIEnv {
 
     // null pointer because the enum cannot be instantiated!
     // otherwise OCIEnv would have been used!
     let mut oci_env = ptr::null_mut();
     let res = unsafe {
         OCIEnvCreate(&mut oci_env,
-                     0 as ub4,
+                     mode as ub4,
                      ptr::null(),
                      ptr::null(),
                      ptr::null(),
@@ -52,7 +48,45 @@ pub fn oci_env_create() -> *mut OCIEnv {
                      0 as size_t,
                      ptr::null_mut())
     };
-    println!("{:?}", res);
+    oci_env
+}
+
+#[link(name = "clntsh")]
+extern "system" {
+    /* create the OCIEnv handle with charset options */
+    fn OCIEnvNlsCreate(env: *mut *mut OCIEnv,
+                       mode: ub4,
+                       ctxp: *mut c_void,
+                       malocfp: Option<extern "system" fn(ctxp: *mut c_void,
+                                                        size: size_t) -> *mut c_void>,
+                       ralocfp: Option<extern "system" fn(ctxp: *mut c_void,
+                                                        memptr: *mut c_void,
+                                                        newsize: size_t) -> *mut c_void>,
+                       mfreefp: Option<extern "system" fn(ctxp: *mut c_void,
+                                                        memptr: *mut c_void)>,
+                       xtramemsz: size_t,
+                       usrmempp: *mut *mut c_void,
+                       charset: ub2,
+                       ncharset: ub2) -> sword;
+
+
+}
+
+pub fn oci_env_nls_create(mode: OCIMode) -> *mut OCIEnv {
+
+    let mut oci_env = ptr::null_mut();
+    let res = unsafe {
+        OCIEnvNlsCreate(&mut oci_env,
+                     mode as ub4,
+                     ptr::null_mut(),
+                     None,
+                     None,
+                     None,
+                     0 as size_t,
+                     ptr::null_mut(),
+                     0,
+                     0)
+    };
     oci_env
 }
 
