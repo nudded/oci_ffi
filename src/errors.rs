0d987a415c692f0ba06433ecbb5225deb22dbdb1
Toon Willems <willemstoon@gmail.com>
2017-01-21 09:12:38 +0000

    add some error handling, tryout some macro stuff

--- src/errors.rs
+++ src/errors.rs
@@ -15,7 +15,6 @@ pub struct OracleError {
 }
 
 impl OracleError {
-
     pub fn new(code: sword, message: &str, location: &str) -> OracleError {
         OracleError {
             error_code: code,
@@ -25,26 +24,59 @@ impl OracleError {
     }
 }
 
+// I'm just having fun with macros here, could have also been 2 functions
+#[macro_export]
+macro_rules! check_error {
+    ($code:expr, $result:expr) => {check_error($code, None, &format!("{}:{}", file!(), line!()), $result)};
+    ($code:expr, $result:expr, $handle:expr) => {check_error($code, Some($handle), &format!("{}:{}", file!(), line!()), $result)};
+}
+
+pub fn check_error<T>(result_code: sword,
+                      handle: Option<*mut OCIHandle>,
+                      location: &str,
+                      result: T)
+                      -> OracleResult<T> {
+    match check_error_code(result_code, handle, location) {
+        None => Ok(result),
+        Some(error) => Err(error),
+    }
+}
+
 /// Result of an oracle function call
 pub type OracleResult<T> = Result<T, OracleError>;
 
 /// convenience function for converting errors and getting more information
-pub fn check_error(result_code: sword, handle: Option<*mut OCIHandle>, location: &str) -> Option<OracleError> {
+pub fn check_error_code(result_code: sword,
+                        handle: Option<*mut OCIHandle>,
+                        location: &str)
+                        -> Option<OracleError> {
     match result_code.into_oci_error() {
         OCIError::OCI_SUCCESS => None,
-        OCIError::OCI_SUCCESS_WITH_INFO => Some(check_error_with_handle(handle, result_code, "Success with info", location)),
+        OCIError::OCI_SUCCESS_WITH_INFO => {
+            Some(check_error_with_handle(handle, result_code, "Success with info", location))
+        }
         OCIError::OCI_NO_DATA => Some(OracleError::new(result_code, "No data", location)),
-        OCIError::OCI_ERROR => Some(check_error_with_handle(handle, result_code, "Error without details", location)),
-        OCIError::OCI_INVALID_HANDLE => Some(OracleError::new(result_code, "Invalid handle", location)),
+        OCIError::OCI_ERROR => {
+            Some(check_error_with_handle(handle, result_code, "Error without details", location))
+        }
+        OCIError::OCI_INVALID_HANDLE => {
+            Some(OracleError::new(result_code, "Invalid handle", location))
+        }
         OCIError::OCI_NEED_DATA => Some(OracleError::new(result_code, "Need data", location)),
-        OCIError::OCI_STILL_EXECUTING => Some(OracleError::new(result_code, "Still executing", location)),
+        OCIError::OCI_STILL_EXECUTING => {
+            Some(OracleError::new(result_code, "Still executing", location))
+        }
         /* this is to be investigated, it seems these cannot be returned from oracle funcations */
         OCIError::OCI_CONTINUE => None,
         OCIError::OCI_ROWCBK_DONE => None,
     }
 }
 
-fn check_error_with_handle(handle: Option<*mut OCIHandle>, result_code: sword, default_msg: &str, location: &str) -> OracleError {
+fn check_error_with_handle(handle: Option<*mut OCIHandle>,
+                           result_code: sword,
+                           default_msg: &str,
+                           location: &str)
+                           -> OracleError {
     if let Some(handle) = handle {
         oci_error_get(handle, location)
     } else {
@@ -62,7 +94,8 @@ extern "system" {
                    errcodep: *mut sb4,
                    bufp: OraText,
                    bufsize: ub4,
-                   error_type: ub4) -> sword;
+                   error_type: ub4)
+                   -> sword;
 
 }
 
@@ -71,7 +104,7 @@ const ERROR_BUF_SIZE: usize = 512;
 fn oci_error_get(handle: *mut OCIHandle, location: &str) -> OracleError {
     let errcodep: *mut sword = &mut 0;
     let mut bufp = Vec::with_capacity(ERROR_BUF_SIZE);
-    let mut error_code: sword;
+    let error_code: sword;
     let mut message = String::from("Error could not be read!");
 
     let res = unsafe {
@@ -93,4 +126,3 @@ fn oci_error_get(handle: *mut OCIHandle, location: &str) -> OracleError {
     };
     OracleError::new(error_code, &*message, location)
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,32 @@ use libc::{c_int, c_ulong, c_ushort, c_void, size_t};
 use std::ptr;
 
 mod types;
+#[macro_use]
 mod errors;
 use types::*;
+
 use errors::*;
 
 #[link(name = "clntsh")]
 extern "system" {
+
     /* create the OCIEnv handle! */
     fn OCIEnvCreate(envp: *mut *mut OCIEnv,
                     mode: ub4,
                     ctxp: *const c_void,
-                    malocfp: *const extern "system" fn(ctxp: *mut c_void,
-                                                       size: size_t) -> *mut c_void,
+                    malocfp: *const extern "system" fn(ctxp: *mut c_void, size: size_t)
+                                                       -> *mut c_void,
                     ralocfp: *const extern "system" fn(ctxp: *mut c_void,
                                                        memptr: *mut c_void,
-                                                       newsize: size_t) -> *mut c_void,
-                    mfreep: *const extern "system" fn(ctxp: *mut c_void,
-                                                      memptr: *mut c_void),
+                                                       newsize: size_t)
+                                                       -> *mut c_void,
+                    mfreep: *const extern "system" fn(ctxp: *mut c_void, memptr: *mut c_void),
                     xtramemsz: size_t,
-                    usermempp: *mut *mut c_void) -> sword;
+                    usermempp: *mut *mut c_void)
+                    -> sword;
 }
 
-pub fn oci_env_create(mode: OCIMode) -> *mut OCIEnv {
+pub fn oci_env_create(mode: OCIMode) -> OracleResult<*mut OCIEnv> {
 
     // null pointer because the enum cannot be instantiated!
     // otherwise OCIEnv would have been used!
@@ -41,7 +45,8 @@ pub fn oci_env_create(mode: OCIMode) -> *mut OCIEnv {
                      0 as size_t,
                      ptr::null_mut())
     };
-    oci_env
+
+    check_error!(res, oci_env)
 }
 
 #[link(name = "clntsh")]
@@ -50,34 +55,34 @@ extern "system" {
     fn OCIEnvNlsCreate(env: *mut *mut OCIEnv,
                        mode: ub4,
                        ctxp: *mut c_void,
-                       malocfp: Option<extern "system" fn(ctxp: *mut c_void,
-                                                        size: size_t) -> *mut c_void>,
+                       malocfp: Option<extern "system" fn(ctxp: *mut c_void, size: size_t)
+                                                          -> *mut c_void>,
                        ralocfp: Option<extern "system" fn(ctxp: *mut c_void,
-                                                        memptr: *mut c_void,
-                                                        newsize: size_t) -> *mut c_void>,
+                                                          memptr: *mut c_void,
+                                                          newsize: size_t)
+                                                          -> *mut c_void>,
                        mfreefp: Option<extern "system" fn(ctxp: *mut c_void,
-                                                        memptr: *mut c_void)>,
+                                                          memptr: *mut c_void)>,
                        xtramemsz: size_t,
                        usrmempp: *mut *mut c_void,
                        charset: ub2,
-                       ncharset: ub2) -> sword;
+                       ncharset: ub2)
+                       -> sword;
 }
 
-pub fn oci_env_nls_create(mode: OCIMode) -> *mut OCIEnv {
-
+pub fn oci_env_nls_create(mode: OCIMode) -> OracleResult<*mut OCIEnv> {
     let mut oci_env = ptr::null_mut();
     let res = unsafe {
         OCIEnvNlsCreate(&mut oci_env,
-                     mode as ub4,
-                     ptr::null_mut(),
-                     None,
-                     None,
-                     None,
-                     0 as size_t,
-                     ptr::null_mut(),
-                     0,
-                     0)
+                        mode as ub4,
+                        ptr::null_mut(),
+                        None,
+                        None,
+                        None,
+                        0 as size_t,
+                        ptr::null_mut(),
+                        0,
+                        0)
     };
-    oci_env
+    check_error!(res, oci_env)
 }
-
